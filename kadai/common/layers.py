3d3718a156ec51bf52189fbf78e102421c59c7b8
Tadashi Nonoyama <30341881+tnonoyama@users.noreply.github.com>
2023-03-23 17:57:46 +0900

    Add files via upload

--- kadai/common/layers.py
+++ kadai/common/layers.py
@@ -1,84 +1,91 @@
+# coding: utf-8
 import numpy as np
-from collections import OrderedDict
+# from common.functions import *
+from common.activations import *
+from common.util import im2col, col2im
+from common.loss import cross_entropy_error
 
-def cross_entropy_error(y, t):
-    """
-    y : 出力値(通常は、0-1の確率)  
-    t : 正解値(通常は、0or1)  
-    """
-    if y.ndim==1:
-        t = t.reshape(1, -1)
-        y = y.reshape(1, -1)
-        
-    batch_size = y.shape[0]
-    delta = 1e-7
-    return -np.sum( t * np.log(y + delta)) / batch_size
-
-def softmax(x):
-    if x.ndim == 2:
-        x = x.T
-        x = x - np.max(x, axis=0)
-        y = np.exp(x) / np.sum(np.exp(x), axis=0)
-        return y.T 
-
-    x = x - np.max(x) # オーバーフロー対策
-    return np.exp(x) / np.sum(np.exp(x))
 
 class ReLU:
     def __init__(self):
         self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+    
+class LeakyReLU:
+    def __init__(self, alpha=0.1):
+        self.mask = None
+        self.alpha = alpha
+        self.mask = None
         
     def forward(self, x):
         self.mask = (x <= 0)
-        out = x.copy() #参照渡しではなく複製する
-        out[self.mask] = 0
+        out = x.copy()
+        out[self.mask] *= self.alpha
         return out
-    
+
     def backward(self, dout):
-        """
-        dout : float, 上流(出力)側の勾配
-        """        
-        dout[self.mask] = 0
-        dLdx = dout
-        return dLdx
+        dout[self.mask] *= self.alpha
+        dx = dout
+        return dx
     
+    
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = sigmoid(x)
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+
+        return dx
+
+
 class Affine:
     def __init__(self, W, b):
         self.W =W
         self.b = b
         
-        # 初期値
         self.x = None
         self.original_x_shape = None
-        self.dW = None # 重みの微分
-        self.db = None # バイアスの微分
+        # 重み・バイアスパラメータの微分
+        self.dW = None
+        self.db = None
 
     def forward(self, x):
-        """
-        順伝播
-        """
-        # 値の保持
+        # テンソル対応(画像形式のxに対応させる)
+        self.original_x_shape = x.shape
+        x = x.reshape(x.shape[0], -1)
         self.x = x
 
-        # 順伝播
         out = np.dot(self.x, self.W) + self.b
 
         return out
 
     def backward(self, dout):
-        """
-        逆伝播
-        dout : float, 上流(出力)側の勾配
-        """
-        # dxは前の層に伝える必要がある
         dx = np.dot(dout, self.W.T)
-        
-        # dWとdbは、勾配法の計算に使われるので、値を保持しておく
         self.dW = np.dot(self.x.T, dout)
-        self.db = np.sum(dout, axis=0) # N個の合計になる
+        self.db = np.sum(dout, axis=0)
         
+        dx = dx.reshape(*self.original_x_shape)  # 入力データの形状に戻す（テンソル対応）
         return dx
-    
+
+
 class SoftmaxWithLoss:
     def __init__(self):
         
@@ -101,137 +108,357 @@ class SoftmaxWithLoss:
         """
         逆伝播
         伝播する値をバッチサイズで割ること
+        dout=1は、他のレイヤと同じ使い方ができるように設定しているダミー変数
         """
         batch_size = self.t.shape[0]
         dx = (self.y - self.t) / batch_size
 
         return dx
 
+class MeanSquaredLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None # 2乗和誤差損失関数の出力
+        self.t = None # 教師データ
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = x #恒等関数
+        self.loss = mean_squared_error(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+        return dx
     
-    
-def numerical_gradient(f, W):
+class Dropout:
     """
-    全ての次元について、個々の次元だけの微分を求める
-    f : 関数
-    W : 偏微分を求めたい場所の座標。多次元配列
+    http://arxiv.org/abs/1207.0580
     """
-    h = 1e-4 # 0.0001
-    grad = np.zeros_like(W)
-    
-    it = np.nditer(W, flags=['multi_index'])
-    
-    while not it.finished:
-        idx = it.multi_index
-        tmp_val = W[idx]
-        
-        W[idx] = tmp_val + h
-        fxh1 = f(W)
-        
-        W[idx] = tmp_val - h 
-        fxh2 = f(W)
-        grad[idx] = (fxh1 - fxh2) / (2*h)
-        
-        W[idx] = tmp_val # 値を元に戻す
-        
-        # 次のindexへ進める
-        it.iternext()   
-        
-    return grad
+    def __init__(self, dropout_ratio=0.5):
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
 
+    def forward(self, x, train_flg=True):
+        if train_flg:
+            self.mask = np.random.rand(*x.shape) > self.dropout_ratio
+            return x * self.mask
+        else:
+            return x * (1.0 - self.dropout_ratio)
 
-class TwoLayerNet:
+    def backward(self, dout):
+        return dout * self.mask
 
-    def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):
-        # 重みの初期化
-        self.params = {}
-        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
-        self.params['b1'] = np.zeros(hidden_size)
-        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size) 
-        self.params['b2'] = np.zeros(output_size)
+class BatchNormalization:
+    def __init__(self, gamma, beta, rho=0.9, moving_mean=None, moving_var=None):
+        self.gamma = gamma # スケールさせるためのパラメータ, 学習によって更新させる.
+        self.beta = beta # シフトさせるためのパラメータ, 学習によって更新させる
+        self.rho = rho # 移動平均を算出する際に使用する減衰率
 
-        # レイヤの生成
-        self.layers = OrderedDict() # 順番付きdict形式. ただし、Python3.6以降は、普通のdictでもよい
-        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
-        self.layers['Relu1'] = ReLU()
-        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
-        self.lastLayer = SoftmaxWithLoss() # 出力層
+        # 予測時に使用する平均と分散
+        self.moving_mean = moving_mean # muの移動平均
+        self.moving_var = moving_var        # varの移動平均
         
-    def predict(self, x):
+        # 計算中に算出される値を保持しておく変数群
+        self.batch_size = None
+        self.x_mu = None
+        self.x_std = None        
+        self.std = None
+        self.dgamma = None
+        self.dbeta = None
+
+    def forward(self, x, train_flg=True):
         """
-        推論関数
-        x : 入力
+        順伝播計算
+        x :  Conv層の場合は4次元、全結合層の場合は2次元  
         """
-        for layer in self.layers.values():
-            # 入力されたxを更新していく = 順伝播計算
-            x = layer.forward(x)
-        
-        return x
-        
-    def loss(self, x, t):
-        """
-        損失関数
-        x:入力データ, t:教師データ
-        """
-        y = self.predict(x)
-        return self.lastLayer.forward(y, t)
-    
-    def accuracy(self, x, t):
-        """
-        識別精度
-        """
-        # 推論. 返り値は正規化されていない実数
-        y = self.predict(x)
-        #正規化されていない実数をもとに、最大値になるindexに変換する
-        y = np.argmax(y, axis=1)
-        
-        if t.ndim != 1 : 
+        if x.ndim == 4:
             """
-            one-hotベクトルの場合、教師データをindexに変換する
+            画像形式の場合
             """
-            t = np.argmax(t, axis=1)
-        
-        # 精度
-        accuracy = np.sum(y == t) / float(x.shape[0])
-        return accuracy
-        
-    def gradient(self, x, t):
+            N, C, H, W = x.shape
+            x = x.transpose(0, 2, 3, 1) # NHWCに入れ替え
+            x = x.reshape(N*H*W, C) # (N*H*W,C)の2次元配列に変換
+            out = self.__forward(x, train_flg)
+            out = out.reshape(N, H, W, C)# 4次元配列に変換
+            out = out.transpose(0, 3, 1, 2) # 軸をNCHWに入れ替え
+        elif x.ndim == 2:
+            """
+            画像形式以外の場合
+            """
+            out = self.__forward(x, train_flg)           
+            
+        return out
+            
+    def __forward(self, x, train_flg, epsilon=1e-8):
         """
-        全パラメータの勾配を計算
+        x : 入力. n×dの行列. nはあるミニバッチのバッチサイズ. dは手前の層のノード数
+        """
+        if (self.moving_mean is None) or (self.moving_var is None):
+            N, D = x.shape
+            self.moving_mean = np.zeros(D)
+            self.moving_var = np.zeros(D)
+                        
+        if train_flg:
+            """
+            学習時
+            """
+            # 入力xについて、nの方向に平均値を算出. 
+            mu = x.mean(axis=0) # 要素数d個のベクトル
+            
+            # 入力xから平均値を引く
+            x_mu = x - mu   # n*d行列
+            
+            # 入力xの分散を求める
+            var = np.mean(x_mu**2, axis=0)  # 要素数d個のベクトル
+            
+            # 入力xの標準偏差を求める(epsilonを足してから標準偏差を求める)
+            std = np.sqrt(var + epsilon)  # 要素数d個のベクトル
+            
+            # 標準化
+            x_std = x_mu / std  # n*d行列
+            
+            # 値を保持しておく
+            self.batch_size = x.shape[0]
+            self.x_mu = x_mu
+            self.x_std = x_std
+            self.std = std
+            self.moving_mean = self.rho * self.moving_mean + (1-self.rho) * mu
+            self.moving_var = self.rho * self.moving_var + (1-self.rho) * var            
+        else:
+            """
+            予測時
+            """
+            x_mu = x - self.moving_mean # n*d行列
+            x_std = x_mu / np.sqrt(self.moving_var + epsilon) # n*d行列
+            
+        # gammaでスケールし、betaでシフトさせる
+        out = self.gamma * x_std + self.beta # n*d行列
+        return out
+
+    def backward(self, dout):
+        """
+        逆伝播計算
+        dout : Conv層の場合は4次元、全結合層の場合は2次元  
+        """
+        if dout.ndim == 4:
+            """
+            画像形式の場合
+            """            
+            N, C, H, W = dout.shape
+            dout = dout.transpose(0, 2, 3, 1) # NHWCに入れ替え
+            dout = dout.reshape(N*H*W, C) # (N*H*W,C)の2次元配列に変換
+            dx = self.__backward(dout)
+            dx = dx.reshape(N, H, W, C)# 4次元配列に変換
+            dx = dx.transpose(0, 3, 1, 2) # 軸をNCHWに入れ替え
+        elif dout.ndim == 2:
+            """
+            画像形式以外の場合
+            """
+            dx = self.__backward(dout)
+
+        return dx
+
+    def __backward(self, dout):
+        
+        """
+        ここを完成させるには、計算グラフを理解する必要があり、実装にかなり時間がかかる.
+        よって、この関数を完成させる作業は、宿題とする.
         """
         
-        # 順伝播
-        self.loss(x, t)
-
-        # 逆伝播
-        dout = 1 # クロスエントロピー誤差を用いる場合は使用されない
-        dout = self.lastLayer.backward(dout=1) # 出力層
+        # betaの勾配
+        dbeta = dout.sum(axis=0)
         
-        ## doutを逆向きに伝える 
-        layers = list(self.layers.values())
-        layers.reverse()
-        for layer in layers:
-            dout = layer.backward(dout)
+        # gammaの勾配(n方向に合計)
+        dgamma = np.sum(self.x_std * dout, axis=0)
+        
+        # Xstdの勾配
+        a1 = self.gamma * dout
+        
+        # Xmuの勾配
+        a2 = a1 / self.std
+        
+        # 標準偏差の逆数の勾配(n方向に合計)
+        a3 = a1 * self.x_mu 
+        
+        # 標準偏差の勾配
+        a4 = -(np.sum(a3, axis=0) ) / (self.std * self.std)
+        
+        # 分散の勾配
+        a5 = 0.5 * a4 / self.std
+        
+        # Xmuの2乗の勾配
+        a6 = a5 / self.batch_size
+        
+        # Xmuの勾配
+        a7 = 2.0  * self.x_mu * a6
+        
+        # muの勾配
+        a8 = -(a2+a7)
 
-        # dW, dbをgradsにまとめる
-        grads = {}
-        grads['W1'], grads['b1'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
-        grads['W2'], grads['b2'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
-
-        return grads
+        # Xの勾配
+        dx = a2 + a7 +  np.sum(a8, axis=0) / self.batch_size # 第3項はn方向に平均
+        
+        self.dgamma = dgamma
+        self.dbeta = dbeta
+        
+        return dx
     
 
-    def numerical_gradient(self, x, t):
-        """
-        勾配確認用
-        x:入力データ, t:教師データ        
-        """
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W # フィルターの重み(配列形状:フィルターの枚数, チャンネル数, フィルターの高さ, フィルターの幅)
+        self.b = b #フィルターのバイアス
+        self.stride = stride # ストライド数
+        self.pad = pad # パディング数
         
-        loss_W = lambda W: self.loss(x, t)
+        # インスタンス変数の宣言
+        self.x = None   
+        self.col = None
+        self.col_W = None
+        self.dcol = None
+        self.dW = None
+        self.db = None
 
-        grads = {}
-        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
-        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
-        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
-        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+    def forward(self, x):
+        """
+        順伝播計算
+        x : 入力(配列形状=(データ数, チャンネル数, 高さ, 幅))
+        """
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = (H + 2*self.pad - FH) // self.stride + 1 # 出力の高さ(端数は切り捨てる)
+        out_w =(W + 2*self.pad - FW) // self.stride + 1# 出力の幅(端数は切り捨てる)
 
-        return grads
+        # 畳み込み演算を効率的に行えるようにするため、入力xを行列colに変換する
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        
+        # 重みフィルターを2次元配列に変換する
+        # col_Wの配列形状は、(C*FH*FW, フィルター枚数)
+        col_W = self.W.reshape(FN, -1).T
+
+        # 行列の積を計算し、バイアスを足す
+        out = np.dot(col, col_W) + self.b
+        
+        # 画像形式に戻して、チャンネルの軸を2番目に移動させる
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.col = col
+        self.col_W = col_W
+
+        return out
+
+    def backward(self, dout):
+        """
+        逆伝播計算
+        Affineレイヤと同様の考え方で、逆伝播させる
+        dout : 出力層側の勾配
+        return : 入力層側へ伝える勾配
+        """
+        FN, C, FH, FW = self.W.shape
+        
+        # doutのチャンネル数軸を4番目に移動させ、2次元配列に変換する
+        dout = dout.transpose(0,2,3,1).reshape(-1, FN)
+
+        # バイアスbはデータ数方向に総和をとる
+        self.db = np.sum(dout, axis=0)
+        
+        # 重みWは、入力である行列colと行列doutの積になる
+        self.dW = np.dot(self.col.T, dout)
+        
+        # (フィルター数, チャンネル数, フィルター高さ、フィルター幅)の配列形状に戻す
+        self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
+
+        # 入力側の勾配は、doutにフィルターの重みを掛けて求める
+        dcol = np.dot(dout, self.col_W.T)
+        
+        # 勾配を4次元配列(データ数, チャンネル数, 高さ, 幅)に変換する
+        dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad, is_backward=True)
+
+        self.dcol = dcol # 結果を確認するために保持しておく
+            
+        return dx
+    
+    
+class MaxPooling:
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+
+        self.pool_h = pool_h # プーリングを適応する領域の高さ
+        self.pool_w = pool_w # プーリングを適応する領域の幅
+        self.stride = stride # ストライド数
+        self.pad = pad # パディング数
+
+        # インスタンス変数の宣言
+        self.x = None
+        self.arg_max = None
+        self.col = None
+        self.dcol = None
+        
+            
+    def forward(self, x):
+        """
+        順伝播計算
+        x : 入力(配列形状=(データ数, チャンネル数, 高さ, 幅))
+        """        
+        N, C, H, W = x.shape
+        
+        # 出力サイズ
+        out_h = (H  + 2*self.pad - self.pool_h) // self.stride + 1 # 出力の高さ(端数は切り捨てる)
+        out_w = (W + 2*self.pad - self.pool_w) // self.stride + 1# 出力の幅(端数は切り捨てる)    
+        
+        # プーリング演算を効率的に行えるようにするため、2次元配列に変換する
+        # パディングする値は、マイナスの無限大にしておく
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad, constant_values=-np.inf)
+        
+        # チャンネル方向のデータが横に並んでいるので、縦に並べ替える
+        # 変換後のcolの配列形状は、(N*C*out_h*out_w, H*W)になる 
+        col = col.reshape(-1, self.pool_h*self.pool_w)
+
+        # 最大値のインデックスを求める
+        # この結果は、逆伝播計算時に用いる
+        arg_max = np.argmax(col, axis=1)
+        
+        # 最大値を求める
+        out = np.max(col, axis=1)
+        
+        # 画像形式に戻して、チャンネルの軸を2番目に移動させる
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.col = col
+        self.arg_max = arg_max
+
+        return out
+
+    def backward(self, dout):
+        """
+        逆伝播計算
+        マックスプーリングでは、順伝播計算時に最大値となった場所だけに勾配を伝える
+        順伝播計算時に最大値となった場所は、self.arg_maxに保持されている        
+        dout : 出力層側の勾配
+        return : 入力層側へ伝える勾配
+        """        
+        
+        # doutのチャンネル数軸を4番目に移動させる
+        dout = dout.transpose(0, 2, 3, 1)
+        
+        # プーリング適応領域の要素数(プーリング適応領域の高さ × プーリング適応領域の幅)
+        pool_size = self.pool_h * self.pool_w
+        
+        # 勾配を入れる配列を初期化する
+        # dcolの配列形状 : (doutの全要素数, プーリング適応領域の要素数) 
+        # doutの全要素数は、dout.size で取得できる
+        dcol = np.zeros((dout.size, pool_size))
+        
+        # 順伝播計算時に最大値となった場所に、doutを配置する
+        # dout.flatten()はdoutを1次元配列に変換している
+        dcol[np.arange(dcol.shape[0]), self.arg_max] = dout.flatten()
+        
+        # 勾配を4次元配列(データ数, チャンネル数, 高さ, 幅)に変換する
+        dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad, is_backward=True)
+        
+        self.dcol = dcol # 結果を確認するために保持しておく
+        
+        return dx
